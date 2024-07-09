56066daa2ea7eb614392cd8d688ce1d2312ab7a5
pedrovsfelix <pedrovitorsf@gmail.com>
2024-07-08 21:25:33 -0300

    feat: project dio java

---
+++ src/main/java/dio/digitalinnovation/gof/service/ClienteService.java
@@ -0,0 +1,16 @@
+package dio.digitalinnovation.gof.service;
+
+import dio.digitalinnovation.gof.model.Cliente;
+
+public interface ClienteService {
+    
+    Iterable<Cliente> buscarTodos();
+
+    Cliente buscarPorId(Long id);
+
+    void inserir(Cliente cliente);
+
+    void atualizar(Long id, Cliente cliente);
+
+    void deletar(Long id);
+}
